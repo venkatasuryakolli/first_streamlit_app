0f7107fed76d479a9228453f50f70463d5336508
venkatasuryakolli <144936864+venkatasuryakolli@users.noreply.github.com>
2023-09-14 13:11:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,13 @@ import requests
 
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 # from urllib.error import URLError
 
 streamlit.header("Fruityvice Fruit Advice!")
